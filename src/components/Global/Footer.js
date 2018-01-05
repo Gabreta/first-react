bf7a6ddb1e73433dc1bbfc0b4a1f0ab0e4bb2f5b
Gabriel <vmgabriel96@gmail.com>
2018-01-04 21:02:21 -0500

    Manejo de Componentes

---
+++ src/components/Global/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/Footer.css'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <p>Contenido hecho por Gabriel Vargas Monroy como una buena pagina web locohona</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
